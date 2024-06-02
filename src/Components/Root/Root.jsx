0601f03813b82ff800f5fc2db480788dc6538700
yousof-ali <yousofali293@gmail.com>
2024-06-03 02:58:34 +0600

    footer added

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,10 +1,12 @@
 
 import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
 
 const Root = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
